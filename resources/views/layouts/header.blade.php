e268f40f0b0a4c0b3c86c5f353f22ff72a977849
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2023-05-16 15:51:16 +0100

    Adjustments to header siderbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
                 <a href="#" class="nk-nav-toggle nk-quick-nav-icon" data-target="sidebarMenu"><em class="icon ni ni-menu"></em></a>
             </div>
             <div class="nk-header-brand d-xl-none">
-                <a href="html/index.html" class="logo-link">
+                <a href="{{route('dashboard')}}" class="logo-link">
                     <img class="logo-light logo-img" src="./images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
                     <img class="logo-dark logo-img" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
                 </a>
